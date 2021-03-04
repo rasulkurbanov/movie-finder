4cf9e6219b3b89842674ef7535b18e2c4768eae6
rasulkurbanov <rasuljohn139@gmail.com>
2021-03-04 18:13:38 +0500

    Add useReducer hook instead of useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
+import Movie from "./components/Movie/Movie";
+
+const MOVIE_API_URL = "https://omdbapi.com/?s=Ali&apikey=a1a0ccac";
+
+//Creating initial state
+let initialState = {
+  loading: true,
+  movies: [],
+  errorMsg: null
+}
+
+//Initializing reducer function
+function reducer(state, action) {
+  switch(action.type) {
+    case "SEARCH_MOVIE_REQUEST": 
+      return {
+        ...state,
+        loading: true,
+        errorMsg: null
+      }
+    case "SEARCH_MOVIE_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload,
+        errorMsg: null
+      }
+    case "SEARCH_MOVIE_ERROR":
+      return {
+        ...state,
+        loading: false,
+        errorMsg: action.error
+      }
+    default:
+      return state  
+  }
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(MOVIE_API_URL)
+      const data = await response.json()
+      
+      if(data.Response === "True") {
+        dispatch({
+          type: "SEARCH_MOVIE_SUCCESS",
+          payload: data.Search
+        })
+       
+      }
+      else {
+        dispatch({
+          type: "SEARCH_MOVIE_ERROR",
+          payload: data.Error
+        })
+      }
+    }
+    fetchData()
+  }, [])
+
+
+
+
+  const search = async (searchValue) => {
+      dispatch({
+        type: "SEARCH_MOVIE_REQUEST"
+      })
+
+      const response = await fetch(
+        `https://omdbapi.com/?s=${searchValue}&apikey=a1a0ccac`
+      );
+      const data = await response.json()
+
+      if(data.Response === "True") {
+        dispatch({
+          type: "SEARCH_MOVIE_SUCCESS",
+          payload: data.Search
+        })
+      }
+      else {
+        dispatch({
+          type: "SEARCH_MOVIE_ERROR",
+          payload: data.Error
+        })
+      }
+  };
+
+  const {loading, errorMsg, movies} = state
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="MOVIE-APP" />
+      <Search search={search} />
+      <div className="movies">
+        {loading && !errorMsg ? (<span>loading...</span>) : errorMsg ? (
+          <div className="errorMessage">{errorMsg}</div> ) : (
+            movies.map((movie, index) => (
+              <Movie key={`${index}-${movie.Title}`} movie={movie} />
+            ))
+        )}
+      </div>
     </div>
   );
 }
